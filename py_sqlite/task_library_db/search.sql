9cd843edc7dcda9ab33064dd8750f09411749966
Bee <113686174+DariaGugnyaeva@users.noreply.github.com>
2025-02-15 11:36:07 +0300

    Create search.sql

---
+++ py_sqlite/task_library_db/search.sql
@@ -0,0 +1,10 @@
+SELECT id,
+    name_book,
+    author,
+    year_created,
+    edition,
+    num_book_case,
+    num_shell,
+    status
+FROM library
+WHERE (name_book LIKE '%' || ? || '%') OR (author LIKE '%' || ? || '%') OR (year_created LIKE '%' || ? || '%');
