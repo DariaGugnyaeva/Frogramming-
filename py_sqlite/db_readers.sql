9b6975b6e1fc0ee31b88fe133f11cdd102992088
Bee <113686174+DariaGugnyaeva@users.noreply.github.com>
2025-02-15 12:23:37 +0300

    Update db_readers.sql

--- py_sqlite/db_readers.sql
+++ py_sqlite/db_readers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE readers (
     id INTEGER PRIMARY KEY,
-    ID_book,
-    num_reader_card,
+    ID_book INTEGER NOT NULL,
+    num_reader_card INTEGER NOT NULL,
     FOREIGN KEY (ID_book)
         REFERENCES library(id)
     FOREIGN KEY (num_reader_card)
