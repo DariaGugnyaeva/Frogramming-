27afce23de14fccc309d1e64526c920772bde5e7
Bee <113686174+DariaGugnyaeva@users.noreply.github.com>
2025-02-15 12:24:38 +0300

    Update db_init.sql

--- py_sqlite/db_init.sql
+++ py_sqlite/db_init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS library (
     id INTEGER PRIMARY KEY,
-    name_book VARCHAR(20),
-    surname VARCHAR(50),
-    author VARCHAR(25),
+    name_book VARCHAR(20) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    author VARCHAR(25) NOT NULL,
     year_created INTEGER,
     edition INTEGER,
-    num_book_case INTEGER,
-    num_shell INTEGER,
+    num_book_case INTEGER NOT NULL,
+    num_shell INTEGER NOT NULL,
     status BOOLEAN DEFAULT FALSE,
-    CHECK ((year_created < 2026) AND (name_book <> ''))
+    CHECK (year_created < 2026)
 );
