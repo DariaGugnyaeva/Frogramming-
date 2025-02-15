931b900afd6502c83beb85c45becb11bc2900ad0
Bee <113686174+DariaGugnyaeva@users.noreply.github.com>
2025-02-15 12:33:22 +0300

    Update all.sql

--- py_sqlite/readers/all.sql
+++ py_sqlite/readers/all.sql
@@ -1,2 +1,6 @@
-SELECT *
-FROM readers;
+SELECT readers.num_reader_card, visitors.surname, visitors.name, library.name_book
+FROM readers
+JOIN visitors
+  ON visitors.num_reader_card = readers.num_reader_card
+JOIN library
+  ON readers.ID_book = library.id;
